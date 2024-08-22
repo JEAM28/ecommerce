a79e6a3aea86293841fb911583803771226d9149
Jesus Angarita <jes.angar11@gmail.com>
2024-08-21 19:00:13 -0500

    avance 1 miercoles

--- ecommerce-jeam28/src/app.module.ts
+++ ecommerce-jeam28/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
